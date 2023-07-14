4f859547cded6c2880585293a0fea651e6f69687
kevinnncr <kacriollo@sudamericano.edu.ec>
2023-07-14 10:49:46 -0500

    tarea s14

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function sumarVariables(a, b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let variable1 = 8;
+  let variable2 = 3;
+  
+  let resultadoSuma = sumarVariables(variable1,variable2);
+  alert(resultadoSuma); 
+  
